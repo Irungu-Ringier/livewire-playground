698c5bfe4bb40242d378ef17349079612f8be69b
Ian Irungu <iani@ringier.co.za>
2023-03-29 20:43:37 +0300

    tutorial files cleanup and setup

--- resources/views/livewire/counter.blade.php
+++
@@ -1,8 +0,0 @@
-<div>
-    <h1 class="text-3xl font-bold underline">
-        Hello world!
-    </h1>
-    {{ $count }}
-    <button wire:click="decrease">-</button>
-    <button wire:click="increase">+</button>
-</div>
