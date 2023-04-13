a4c6bf42267a6f849012602818d41fed3962d3f7
Ian Irungu <iani@ringier.co.za>
2023-04-13 20:46:39 +0300

    Add sort icons

--- resources/views/livewire/data-table.blade.php
+++ resources/views/livewire/data-table.blade.php
@@ -37,30 +37,44 @@
                     <thead>
                     <tr>
                         <th
-                            class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                            <div class="flex">
+                            class="px-6 py-3">
+                            <button wire:click="sortBy('name')" class="flex bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                 <span>Name</span>
-                                <button wire:click="sortBy('name')">
-                                    <svg class="h-4 w-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                              d="M5 5a1 1 0 011.707 0L10 8.586 13.293 5.3A1 1 0 1114.707 6.7l-4 4a1 1 0 01-1.414 0l-4-4A1 1 0 015 5z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                </button>
-                            </div>
+                                @if($sortField === 'name')
+                                    @if($sortAsc)
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 mb-1">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+
+                                    @else
+                                        <div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 mb-1">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                                            </svg>
+                                        </div>
+                                    @endif
+                                @endif
+                            </button>
                         </th>
                         <th
-                            class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
-                            <div class="flex">
+                            class="px-6 py-3">
+                            <button wire:click="sortBy('email')" class="flex bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
                                 <span>Email</span>
-                                <button wire:click="sortBy('email')">
-                                    <svg class="h-4 w-4 text-gray-400" fill="currentColor" viewBox="0 0 20 20">
-                                        <path fill-rule="evenodd"
-                                              d="M5 5a1 1 0 011.707 0L10 8.586 13.293 5.3A1 1 0 1114.707 6.7l-4 4a1 1 0 01-1.414 0l-4-4A1 1 0 015 5z"
-                                              clip-rule="evenodd"></path>
-                                    </svg>
-                                </button>
-                            </div>
+                                @if($sortField === 'email')
+                                    @if($sortAsc)
+                                        <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 mb-1">
+                                            <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 8.25l-7.5 7.5-7.5-7.5" />
+                                        </svg>
+
+                                    @else
+                                        <div>
+                                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 20 20" stroke-width="1.5" stroke="currentColor" class="w-3 h-3 mb-1">
+                                                <path stroke-linecap="round" stroke-linejoin="round" d="M4.5 15.75l7.5-7.5 7.5 7.5" />
+                                            </svg>
+                                        </div>
+                                    @endif
+                                @endif
+                            </button>
                         </th>
                         <th
                             class="px-6 py-3 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">
